17aba18673247b3ff3492c596b8383d997809842
TathaiSardar01 <91777520+TathaiSardar01@users.noreply.github.com>
2022-08-26 15:02:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var first_name = "Sushant ";
+var second_name = "Singh Rajput";
+
+var full_name = first_name.concat(second_name);
+console.log(full_name);
+
+var a= 10;
+var b= 5;
+console.log(a+b);
